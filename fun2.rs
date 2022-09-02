57ebcb3bbd210437e97fb1eb96a3872c8971da03
Zohaib <zrana1214@gmail.com>
2022-09-01 23:53:45 -0400

    argument modification

---
+++ fun2.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let mut res = 0.0;
+    modifies(&mut res); // this will change the value of res to 1.0 
+    println!("res is {}", res);
+}
+
+// this function takes in a mutable reference, allowing us to modify its argument
+fn modifies(x: &mut f64){
+    *x = 1.0
+}
