2ecdcde904b6ac788f88b0ee12fdab4c9580d756
Zohaib <zrana1214@gmail.com>
2022-09-01 23:13:25 -0400

    loops and if-statements

--- let2.rs
+++ let2.rs
@@ -15,5 +15,10 @@ fn main(){
         }
     }
 
+    // this does the same this as the for loop above
+    for i in 0..5{
+        let even_odd = if i % 2 == 0 {"even"} else {"odd"};
+        println!("{} {}", even_odd, i);
+    }
 
 }
