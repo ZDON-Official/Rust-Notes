f9af2d0dda98d061ded941db1515b113ff2b0323
Zohaib <zrana1214@gmail.com>
2022-09-01 23:29:37 -0400

    addition for int and float

--- Add.rs
+++ Add.rs
@@ -5,5 +5,11 @@ fn main(){
     }
 
     println!("the sum is {}", sum);
+    
+    let mut sum = 0.0; // <- for floats
+    for i in 0..5{
+        sum += i as f64; // this tell rust to convert int to float
+    }
 
+    println!("the sum is {}", sum);
 }
