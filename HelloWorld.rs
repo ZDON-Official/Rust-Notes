66f335b8cfe4d3ecb1b7163581ec35709c83a730
Zohaib <zrana1214@gmail.com>
2022-09-01 22:56:03 -0400

    added varibales

--- HelloWorld.rs
+++ HelloWorld.rs
@@ -1,3 +1,22 @@
 fn main(){
     println!("Hello World!");
+
+    /*
+        the ! in the println means that this is a macro all 
+        macros is a way of writing code that writes other code
+    */
+
+    // ! variables 
+    let answer = 42;
+    println!("this is a varible {}", answer);
+
+
+
+
+
+
+
+
+
+
 }
