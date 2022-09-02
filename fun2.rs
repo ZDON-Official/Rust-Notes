1ad22a8b3a04afe1dd3e6ccbfe2392a6c9ddd403
Zohaib <zrana1214@gmail.com>
2022-09-01 23:57:05 -0400

    argument modification

--- fun2.rs
+++ fun2.rs
@@ -2,9 +2,14 @@ fn main(){
     let mut res = 0.0;
     modifies(&mut res); // this will change the value of res to 1.0 
     println!("res is {}", res);
+        
+    
+    // ! using type-after-variable for let
+    let bigInt: i64 = 0; 
 }
 
 // this function takes in a mutable reference, allowing us to modify its argument
 fn modifies(x: &mut f64){
     *x = 1.0
 }
+
