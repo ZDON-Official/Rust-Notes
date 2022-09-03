0db37db808f47755fd98915dcf8dc90363647d78
Zohaib <zrana1214@gmail.com>
2022-09-02 22:03:35 -0400

    vec! macro

---
+++ Vectors2.rs
@@ -0,0 +1,21 @@
+/*
+    ! More about vectors 
+    Vec! is macro that can be used for initializing a vector
+    * .push() - add values at the end 
+    * .pop() - remove value from the end
+*/
+
+fn main(){
+    let mut v1 = vec![10,20,30,40];
+    v1.pop(); // will remove 40 from the vector
+
+    let mut v2 = Vec::new();
+    v2.push(10);
+    v2.push(20);
+    v2.push(30);
+
+    assert_eq!(v1, v2);
+
+    v2.extend(0..2);
+    assert_eq!(v2, &[10, 20, 30, 0, 1]);
+}
