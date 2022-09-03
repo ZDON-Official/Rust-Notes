234f7f8764459d0673515b879f4732b93df85358
Zohaib <zrana1214@gmail.com>
2022-09-02 21:05:54 -0400

    slice.get()

--- Slices_Dice.rs
+++ Slices_Dice.rs
@@ -1,7 +1,7 @@
 /*
     ! Slicing and Dicing
     {} cannot be used to print an Array, instead debug print is used
-    Arrays can only contain values of ONE TYPE <-----------------------
+    Arrays can only contain values of // ! ONE TYPE
 */
 
 fn main(){
@@ -17,13 +17,27 @@ fn main(){
     
     println!("");
 
-    // ! slice
+    // ! Slicing and Dicing
     let slice1 = &ints[0..2]; // only the first to not including index 2
     let slice2 = &ints[1..]; // from index 1 till end
     
     println!("ints {:?}", ints);
     println!("slice1 {:?}", slice1);
     println!("slice2 {:?}", slice2);
-    
+    println!("");
 
+    // this is similar to python slices, except a copy is never made
+    // rather, the slices all borrow their data from their arrays
+
+    // ! Optional values
+    // there is no try-catch block to wrap code that may panic, instead 
+    // Rust has a 'get' method for slices that does not panic
+
+    let ints1 = [1,2,3,4,5];
+    let slice_get = &ints; // a slice of the array ints1
+    let first = slice_get.get(0); // get the first element 
+    let last = slice_get.get(5); // ! this line return none since 5 is out of bound 
+
+    println!("first {:?}", first);
+    println!("last {:?}", last);
 } 
